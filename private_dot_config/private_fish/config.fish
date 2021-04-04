cbc47fefedf3b00dba9d7f9800132d720d5f7fbd
JamelleG <jamelleg@outlook.com>
2021-04-04 21:45:07 +0100

    use official path function

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,15 +2,12 @@ source $HOME/.config/fish/aliases.fish
 source $HOME/.config/fish/git.fish
 source $HOME/.config/fish/docker.fish
 source $HOME/.config/fish/podman.fish
+
 set -gx SXHKD_SHELL '/usr/bin/sh'
 set -Ux EDITOR nvim 
 set -Ux VISUAL nvim 
 
-function addpath
-  contains $argv[1] $fish_user_paths; or set -Ua fish_user_paths $argv[1]
-end
-
-addpath $HOME/.cargo/bin
-addpath /home/linuxbrew/.linuxbrew/bin
+fish_add_path $HOME/.cargo/bin
+fish_add_path /home/linuxbrew/.linuxbrew/bin
 
 starship init fish | source
