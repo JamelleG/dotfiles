cda96574347ea44328e27f6452aaca70f5837e79
Jamelle Gordon <jamelleg@outlook.com>
2026-03-28 14:19:32 +0000

    update polybar

--- private_dot_config/polybar/executable_launch.sh
+++ private_dot_config/polybar/executable_launch.sh
@@ -8,6 +8,9 @@ polybar-msg cmd quit
 
 mkdir -p /tmp/polybar
 
+export POLY_BAT=$(ls -1 /sys/class/power_supply/ 2>/dev/null | grep -E '^BAT' | head -n 1)
+export POLY_ADP=$(ls -1 /sys/class/power_supply/ 2>/dev/null | grep -E '^AC|^ADP' | head -n 1)
+
 polybar topbar 2>&1 | tee /tmp/polybar/topbar.log & disown
 
 echo "Polybar launched successfully! Logs saved to /tmp/polybar/topbar.log"
